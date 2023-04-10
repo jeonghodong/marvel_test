1ffdbb53b41894a9cecf45cc2ba5d1a487e398b7
Hodong <fire13764@gmail.com>
2023-04-11 00:58:19 +0900

    recoilRoot 루트파일에다가 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Result from "./pages/Result";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/marvel_test" element={<Home />} />
-        <Route path="/1" element={<Detail />} />
-        <Route path="/Result" element={<Result />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/marvel_test" element={<Home />} />
+          <Route path="/1" element={<Detail />} />
+          <Route path="/Result" element={<Result />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
